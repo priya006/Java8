149805aeac66774c25702f9284f78929127fe8bd
pboopathi <pboopathi@ebay.com>
2020-09-29 10:34:41 -0700

    Added code for Generics Method

---
+++ src/com/GenericMethod.java
@@ -0,0 +1,21 @@
+package com;
+
+public class GenericMethod {
+    public static void main(String[] args) {
+
+        Integer[] intArray = {1,2,3};
+        Character[] charArray = {'A','B','c'};
+
+        printArray(intArray);
+        printArray(charArray);
+    }
+
+    public static  <E>  void printArray(E[] arrayElemets){
+
+        for(E element: arrayElemets){
+            System.out.println(element);
+        }
+
+    }
+
+}
