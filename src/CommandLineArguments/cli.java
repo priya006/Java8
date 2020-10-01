0c9c04852a206b775e392d59fff211d0a57aef83
pboopathi <pboopathi@ebay.com>
2020-10-01 12:12:08 -0700

    Updated commandline args

---
+++ src/CommandLineArguments/cli.java
@@ -0,0 +1,19 @@
+package CommandLineArguments;
+
+//During javac compile java program -> Byte code -> Class file
+//During java javaprogram.java -> The actual class file gets executed
+
+public class cli {
+
+    public static void main(String[] args) {
+
+        System.out.println("Command-Line arguments are");
+
+        // loop through all arguments
+        for(String str: args) {
+            System.out.println(str);
+        }
+    }
+
+    }
+
