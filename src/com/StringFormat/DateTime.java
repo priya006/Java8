8b8b33d428e56c5260e14d01ee860f2614c91dee
pboopathi <pboopathi@ebay.com>
2020-10-02 11:04:16 -0700

    Local time month and year is calculated

--- src/com/StringFormat/DateTime.java
+++ src/com/StringFormat/DateTime.java
@@ -8,11 +8,12 @@ public class DateTime {
         LocalDateTime localDateTime = LocalDateTime.now();
         System.out.println(localDateTime);
 
-        //Flag =  %ta
+        //Flag =  %ta %t is %t is the prefix for Date/Time conversions.
         System.out.println(String.format("The current week day in lowercase is %tA",localDateTime));
         System.out.println(String.format("The current week day is %TA",localDateTime));
         System.out.printf("The current week day is %Ta using printf",localDateTime);
 
         System.out.println(String.format("%n The current Year is : %1$tY  current month: %1$tm",localDateTime));
+
     }
 }
