b64f7abfbd3cce550f5d185a5282ffe810c53d5d
pboopathi <pboopathi@ebay.com>
2020-10-01 22:01:26 -0700

    Formatted

--- src/com/StringFormat/StringFormat.java
+++ src/com/StringFormat/StringFormat.java
@@ -25,15 +25,16 @@ public class StringFormat {
         String withnewline = String.format("The name is %3$s %2$s %1$s %n %1$s ", firstName, middleName, lastName);
         System.out.println(withnewline);
 
-        int number;
-
-        System.out.println(String.format("This is a number %d", 1234));
+        System.out.println(String.format("4 digit number %d", 1234));
         System.out.println(String.format("This is a number %d", 1));
         //width is added as 50
-        System.out.println(String.format("This is a number %20d", 1234));
+        System.out.println(String.format("Width is 20 %20d", 1234));
         //Flag ',' is added
-        System.out.println(String.format("This is a number %,d", 1234343434));
-
+        System.out.println(String.format("Flag ',' is added %,d", 1234343434));
+        //Locale.ITALY includes decimal instead of ,
+        System.out.println(String.format(Locale.ITALY,"Italy %,d", 1234343434));
+        //Left justified with -
+        System.out.println(String.format("Left justified %-20d", 1234));
 
     }
 }
